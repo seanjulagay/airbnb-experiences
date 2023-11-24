bd03fe530c49504c61c4f93c374b56f666173320
Sean <63425198+seanjulagay@users.noreply.github.com>
2023-11-25 02:52:06 +0800

    Dynamic rating desc done

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -1,17 +1,44 @@
 import "../styles/Card.scss";
-import blankPhoto from "../images/blank-person-photo.jpg";
+import imgBlank from "../images/blank-person-photo.jpg";
+import imgStar from "../images/star.png";
 
-export default function Card({ status = "Placeholder", image = blankPhoto }) {
+export default function Card({
+  status,
+  image = imgBlank,
+  rating = "0.0",
+  ratingAmount = 0,
+  location = "No Location",
+  desc = "Placeholder",
+  price = 0,
+}) {
+  let cardStatus = null;
+
+  if (status) {
+    cardStatus = (
+      <div className="card-status-div">
+        <span className="card-status">{status}</span>
+      </div>
+    );
+  }
+
   return (
     <div className="card">
       <div className="card-container">
         <div className="card-img-container">
-          <span className="card-status">{status}</span>
+          {cardStatus}
           <div className="card-img-div">
             <img src={image} alt="" className="card-image" />
           </div>
         </div>
-        <div className="card-desc-container"></div>
+        <div className="card-desc-container">
+          <div className="card-desc-rating-container">
+            <img src={imgStar} alt="" className="card-desc-star-img" />
+            <span className="card-desc-rating-text">{rating}</span>
+            <span className="card-desc-rating-details-text">
+              ({ratingAmount}) • {location}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/assets/components/Gallery.jsx
+++ src/assets/components/Gallery.jsx
@@ -1,11 +1,15 @@
 import Card from "./Card";
 import "../styles/Gallery.scss";
+import imgLifeLessons from "../images/katie-zaferes.png";
+import imgWeddingPhotog from "../images/wedding-photography.png";
+import imgMountainBiking from "../images/mountain-biking.png";
 
 export default function Gallery() {
   return (
     <div className="gallery">
-      <Card />
-      <Card />
+      <Card status="sold out" image={imgLifeLessons} />
+      <Card status="online" image={imgWeddingPhotog} />
+      <Card status="" image={imgMountainBiking} />
       <Card />
     </div>
   );
